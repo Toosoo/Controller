0133c93900698eeef99001852357a086f1b75e0a
Toosoo <hello@iamtoso.com>
2023-12-17 12:57:01 +0200

    f

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Canvas shadows camera={{ near: 0.1, far: 50, position: [0, 0, 7] }} >
+    <Canvas shadows camera={{ near: 0.1, far: 50, position: [0, 0, 7] }} className="r3f">
       <App />
     </Canvas>
   </React.StrictMode>
